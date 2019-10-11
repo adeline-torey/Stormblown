e5f045031bca81aa70a4163c423bf7981b7b70f6
Adeline Torey <55276131+AdelineTorey@users.noreply.github.com>
2019-10-11 16:29:34 +0200

    Add files via upload

---
+++ functions/scorep.R
@@ -0,0 +1,44 @@
+## My third R function
+## Score reporter - input marks (0-100) to return a report on score description and pass/fail
+
+scogen <- function(marks){
+  if(marks >= 81 & marks <= 100)
+    score <- 5
+  if(marks >= 61 & marks <= 80)
+    score <- 4
+  if(marks >= 41 & marks <= 60)
+    score <- 3
+  if(marks >= 21 & marks <= 40)
+    score <- 2
+  if(marks >= 0 & marks <= 20)
+    score <- 1
+  return(score)
+}
+
+
+scodes <- function(score){
+  if(!is.numeric(score))
+    stop("'score' must be numeric")
+  if(score < 1 | score > 5)
+    stop("'score' only accepts input between 1-5")
+  switch(score,
+         '1' = print("Very poor"),
+         '2' = print("Poor"),
+         '3' = print("Average"),
+         '4' = print("Good"),
+         '5' = print("Very good"))
+  if (score >2){
+    print("Pass")
+  } else
+    print("Fail")
+}
+
+scorep <- function(marks){
+  scocat <- scogen(marks)
+  result <- scodes(scocat)
+  return(result)
+}
+
+## Example
+
+scorep(63) 

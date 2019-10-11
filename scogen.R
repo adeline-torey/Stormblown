733e624d0ead9d99cc1f5ccd25d894534b727d5e
Adeline Torey <55276131+AdelineTorey@users.noreply.github.com>
2019-10-11 12:59:00 +0200

    Add files via upload

---
+++ scogen.R
@@ -0,0 +1,28 @@
+## My second R function
+## Score generator - converts marks (0-100) to a score (1-5)
+
+scogen <- function(marks){
+  if(marks >= 81 & marks <= 100)
+    score <- 1
+  if(marks >= 61 & marks <= 80)
+    score <- 2
+  if(marks >= 41 & marks <= 60)
+    score <- 3
+  if(marks >= 21 & marks <= 40)
+    score <- 2
+  if(marks >= 0 & marks <= 20)
+    score <- 1
+  return(score)
+}
+
+
+## Examples
+
+scogen(82)
+scogen(81)
+scogen(80)
+scogen(45)
+scogen(20)
+scogen(19)
+scogen(21)
+scogen(0)
